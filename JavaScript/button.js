3ecaa5362f312ba35be247216c80321bbe23685d
A1oy <s10223380@connect.np.edu.sg>
2021-12-01 22:39:12 +0800

    a

---
+++ JavaScript/button.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("button");
+
+button.onclick = function(){
+    document.getElementById("text").innerHTML = "Your response has been submitted. Thanks for contacting me!";
+}
